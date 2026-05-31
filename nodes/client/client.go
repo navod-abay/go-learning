61bcf1f37e106aca6bd69a599b65aa0207c4ba2a
navod-abay <emil.navod@gmail.com>
2026-06-01 00:18:14 +0530

    Mutex to restrict only one master per client

--- nodes/client/client.go
+++ nodes/client/client.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
 	"net"
 	"sync"
+
+	sharedproto "github.com/navod-abay/mandelbrotset-go/nodes/shared_proto"
 )
 
 const port string = ":8080"
@@ -20,16 +24,48 @@ func handleStringMessage(conn net.Conn, err error, keepListening *bool) (string,
 	return "", err
 }
 
+func handleConnection(conn net.Conn, lock *sync.Mutex) error {
+	lock.Lock() // only one master can push work into the client
+	defer lock.Unlock()
+	for true {
+		bufReader := bufio.NewReader(conn)
+		n, err := bufReader.Peek(1)
+		if err != nil {
+			continue // Blocking if the stream is empty
+		}
+		if len(n) > 0 {
+			parse_err := parseMessage(bufReader)
+			if parse_err != nil { // Close TCP connection if fail to parse
+				return parse_err
+			}
+		}
+	}
+	return nil
+}
+
+func parseMessage(buffer *bufio.Reader) error {
+	msgType, err := sharedproto.ReadHeader(buffer)
+	if err != nil {
+		switch {
+		case errors.Is(err, sharedproto.ErrIncorrectMessage):
+
+		}
+	}
+	return nil
+}
+
 func KeepListening(ln net.Listener, wg *sync.WaitGroup) {
 	defer wg.Done()
+	var lock sync.Mutex
 	fmt.Printf("Started Listening on port: %v\n", port)
 	keepConnection := true
 	for keepConnection {
-		conn, err := ln.Accept()
+		conn, err := ln.Accept() // Accept a connection
 		if err != nil {
 			fmt.Printf("Error accepting TCP message, err: %v ", err)
 			continue
 		}
+		go handleConnection(conn, &lock)
 		handleStringMessage(conn, err, &keepConnection)
 	}
 }
